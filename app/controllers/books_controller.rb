028479df2e2716025a68442fac058c0880aab6bf
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-03 04:24:31 +0000

    [Add]updateアクションにバリデーションチェックを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,10 +25,14 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(list_params)
-    flash[:notice]="Book was successfully updated."
-    redirect_to book_path(book.id)
+    # ssssss
+    @book=Book.find(params[:id])
+    if @book.update(list_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
