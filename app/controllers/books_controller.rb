34b9c620a0ed34fb9bf4ecacbfa37e6e05573833
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-04-03 03:38:02 +0000

    [Start]createアクションのバリデーションチェックを実装

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+
+  def index
+    @books=Book.all
+    @book=Book.new
+  end
+
+  def show
+    @book=Book.find(params[:id])
+  end
+
+  def edit
+    @book=Book.find(params[:id])
+  end
+
+  def create
+    @books=Book.all
+    @book=Book.new(list_params)
+    if @book.save
+      flash[:notice]="Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  def update
+    book=Book.find(params[:id])
+    book.update(list_params)
+    flash[:notice]="Book was successfully updated."
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+
+private
+
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
